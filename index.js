14f16a3445357f2a05005cfa53d439811dd376f9
marian.eq <mariano.eq@gmail.com>
2022-05-06 17:33:07 +0100

    functionalities updated

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ let message = "";
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
 let messageEl = document.getElementById("message-el");
-
+let count = 5;
+console.log("count:  ", count)
 function getRandomNumbers() {
   //get 2 random numbers and assing them to 2 dif var.
   let randomNumber = Math.floor(Math.random() * 11) + 1;
 
   //if randomNumber 1 0R 11 ask the player what they prefer.
-  
+
   return randomNumber;
 }
 
 function startGame() {
   isAlive = true;
+  hasBlackJack=false;
   let firstCard = getRandomNumbers();
   let secondCard = getRandomNumbers();
   cards = [firstCard, secondCard];
+  console.log("CARDS:  ", cards);
   sum = firstCard + secondCard;
-  console.log("Array: ", cards);
-  console.log(firstCard, secondCard);
-  console.log("sum: ", sum);
-  console.log(cards);
   displayCards();
+  counter();
   // Sum those values and render in the "sum" p elem
   // find out if "sum" is less, greater or equal to 21
 }
@@ -34,25 +34,31 @@ function startGame() {
 //Render those 2 var to the "Cards" paragraph elem
 function displayCards() {
   cardsEl.textContent = "Cards: ";
+
   // Loop through cards, render each element in cards-el element.
   for (let i = 0; i < cards.length; i++) {
-    cardsEl.textContent += cards[i] + " ";
+    cardsEl.textContent += cards[i] + "    ";
   }
 
   sumEl.textContent = "Sum: " + sum;
 
-  if (sum <= 20) {
+  if (sum <= 19) {
     isAlive = true;
-    message = "Do you want to draw a new card?";
+    message = "C'mon ask for a card!";
+  } else if (sum === 20) {
+    isAlive = true;
+    message = "You've to be extremely lucky to get an AS, Let's see!";
   } else if (sum === 21) {
     hasBlackJack = true;
+    count = 5
     message = "You've got Blackjack!!!";
-    hasBlackJack = true;
+    alert("Today everything is gonna go well!! You won!!!");
   } else {
-    message = "You're out of the game!";
+    message = `You are not that lucky today! Start the game again!`;
     isAlive = false;
   }
-
+  console.log("is Alive: ",isAlive);
+  console.log("hasBlackjack:  ",hasBlackJack);
   messageEl.textContent = message;
 }
 
@@ -62,7 +68,23 @@ function askCard() {
     cards.push(newCard);
     sum += newCard;
     displayCards();
+   
   }
+}
+//Every time start game is pressed the counter decrease by one.
+function counter() {
+
+  let currentCounter = count--;
+  console.log(`currentCounter:  `, currentCounter);
+  let counterEl = document.getElementById("counter-el");
+  counterEl.textContent = `REMAINDER TIMES TO WIN: ${currentCounter}`;
+  if (count < 0 ) {
+    alert("Game Over");
+    count = 5;
+    isAlive =false;
+  } 
 }
 
 
+//If I've got blackjack before 5 attempts I have to reset the counter up to 5 again.
+// after the winner alert when I click start game it has to reser the counter
